db860917b3d7555cac0696ef3de635609d3e1b9c
Harttle <yangjun14@baidu.com>
2019-07-11 21:28:36 +0800

    modified:   server.js

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ var numClients = 0
 
 io.on('connection', (socket) => {
   numClients++
+  console.log(`client join, ${numClients} in total`)
   socket.broadcast.emit('client join', numClients)
 
-  socket.on('content', (data) => {
-    socket.broadcast.emit('content', data)
+  socket.on('message', ({ mime, data }) => {
+    console.log(`${mime} received: ${data.length} bytes`)
+    socket.broadcast.emit('image', data)
   })
 
   socket.on('disconnect', () => {
+    console.log(`client left, ${numClients} remaining`)
     socket.broadcast.emit('client left', numClients)
   })
 })
