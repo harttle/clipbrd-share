24b5a76472af8af02bc9fa4288029daaebae9c8b
harttle <yangjvn@126.com>
2019-07-12 19:44:11 +0800

    refactor: use buffer compare

--- src/client.js
+++ src/client.js
@@ -1,23 +1,24 @@
 const io = require('socket.io-client')
 const POLL_INTERVAL = 1000
 
-let lastContent = { mime: 'text/plain', data: Buffer.alloc(0) }
+let lastMsg = { mime: 'text/plain', data: Buffer.alloc(0) }
 
 exports.connect = function (url, clipboard) {
   console.log(`connecting to ${url}...`)
   const socket = io(url)
 
   setInterval(async function () {
-    const content = await clipboard.read()
-    if (isChanged(content)) {
-      lastContent = content
-      console.log(`clipboard changed, sending ${content.data.length} bytes of ${content.mime}`)
-      socket.emit('message', content)
+    const msg = await clipboard.read()
+    if (compare(msg, lastMsg)) {
+      lastMsg = msg
+      console.log(`clipboard changed, sending ${msg.data.length} bytes of ${msg.mime}`)
+      socket.emit('message', msg)
     }
   }, POLL_INTERVAL)
 
   socket.on('message', msg => {
     console.log(`clipboard received ${msg.data.length} bytes of ${msg.mime}`)
+    lastMsg = msg
     clipboard.write(msg)
   })
   socket.on('connect', () => console.log('connected'))
@@ -29,7 +30,7 @@ exports.connect = function (url, clipboard) {
   socket.on('client join', (count) => console.log(`client join, ${count} client in total`))
 }
 
-function isChanged (content) {
-  return content.mime !== lastContent.mime ||
-    Buffer.compare(content.data, lastContent.data)
+function compare (msg, lastMsg) {
+  return msg.mime !== lastMsg.mime ||
+    Buffer.compare(msg.data, lastMsg.data)
 }
