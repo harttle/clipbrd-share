80a2e83f2c94f6e732421b97f55135be389cd8bd
harttle <yangjvn@126.com>
2019-12-09 03:17:46 +0800

    fix: respect to config file

--- bin/server.js
+++ bin/server.js
@@ -3,8 +3,8 @@
 const conf = require('../src/config')
 const server = require('../src/server')
 
-const port = process.env.PORT || 3000
-const host = process.env.HOST || '0.0.0.0'
+const port = process.env.PORT || conf.get('port') || 3000
+const host = process.env.HOST || conf.get('host') || '0.0.0.0'
 
 conf.set('port', port)
 conf.set('host', host)
